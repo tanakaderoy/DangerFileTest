bee1499031f5ef929ff7effa27f6cfd70af713a0
tanakaderoy <42681052+tanakaderoy@users.noreply.github.com>
2021-03-11 13:51:10 -0500

    :pencil2: update danger

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,18 @@
 import { message, danger } from 'danger';
+import fs from 'fs';
 
 const modifiedMD = danger.git.modified_files.join('- ');
 message('Changed Files in this PR: \n - ' + modifiedMD);
+
+const modifiedFiles = danger.git.modified_files;
+
+modifiedFiles.forEach(file => {
+  try {
+    const data = fs.readFileSync(file, 'utf-8');
+    if (data.includes('Tanaka')) {
+      message(`This file: ${file} includes Tanaka in it`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
